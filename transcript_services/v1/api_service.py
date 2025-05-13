1d3e194ef832ebfa24f7d83e9fb7cf452f4bde26
vanyabrucker <vanyabrucker@gmail.com>
2025-05-13 17:51:27 +0200

    Handle recording-failure

--- transcript_services/v1/api_service.py
+++ transcript_services/v1/api_service.py
@@ -20,7 +20,7 @@ def start_transcription(transcript_uuid):
         raise ValueError("API key is not set in environment variables.")
 
     # API endpoint
-    url = os.getenv("TRANSCRIPT_API_URL")
+    url = os.getenv("TRANSCRIPT_API_URL") + "/v1/record/done"
 
     if not url:
         raise ValueError("API URL is not set in environment variables.")
@@ -43,6 +43,42 @@ def start_transcription(transcript_uuid):
         print(f"Successfully started transcription for UUID: {transcript_uuid}")
     else:
         print(f"Error starting transcription: {response.status_code}")
+        print(f"Response: {response.text}")
+
+    return response
+
+
+def could_not_record(transcript_id):
+    # API credentials
+    api_key = os.getenv("TRANSCRIPT_API_KEY")
+
+    if not api_key:
+        raise ValueError("API key is not set in environment variables.")
+
+    # API endpoint
+    url = os.getenv("TRANSCRIPT_API_URL") + "/v1/record/failed"
+
+    if not url:
+        raise ValueError("API URL is not set in environment variables.")
+
+    # Request headers
+    headers = {
+        "x-api-key": api_key,
+    }
+
+    # Request parameters
+    params = {
+        "transcript_id": transcript_id
+    }
+
+    # Send POST request
+    response = requests.post(url, headers=headers, params=params)
+
+    # Check if request was successful
+    if response.status_code == 200:
+        print(f"Successfully informed of recording failure for UUID: {transcript_id}")
+    else:
+        print(f"Error informing of recording failure: {response.status_code}")
         print(f"Response: {response.text}")
 
     return response
