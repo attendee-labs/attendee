7f6ed14872487d32798fd687c054d7de82acdcc7
Nick Venenga <nick.venenga@15five.com>
2025-10-17 10:07:02 -0400

    wip

--- attendee/celery.py
+++ attendee/celery.py
@@ -2,7 +2,6 @@ import os
 import ssl
 
 from celery import Celery
-from celery.signals import setup_logging
 
 # Set the default Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attendee.settings")
@@ -22,12 +21,3 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Auto-discover tasks from all registered Django apps
 app.autodiscover_tasks()
-
-
-@setup_logging.connect
-def config_loggers(*args, **kwargs):
-    from logging.config import dictConfig
-
-    from django.conf import settings
-
-    dictConfig(settings.LOGGING)
