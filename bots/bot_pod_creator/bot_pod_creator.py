efa2206b7148dd2b9949138408ab5f22b9187d09
Noah Duncan <crusty.noah@gmail.com>
2025-11-24 16:14:57 -0600

    add bot pod data

--- bots/bot_pod_creator/bot_pod_creator.py
+++ bots/bot_pod_creator/bot_pod_creator.py
@@ -273,8 +273,8 @@ class BotPodCreator:
         ]
 
     def apply_spec_to_bot_pod(self, bot_pod: client.V1Pod) -> dict:
-        bot_pod_spec_data = self.api_client.sanitize_for_serialization(bot_pod)
-        return apply_json6902_patch(bot_pod_spec_data, self.bot_pod_spec)
+        bot_pod_data = self.api_client.sanitize_for_serialization(bot_pod)
+        return apply_json6902_patch(bot_pod_data, self.bot_pod_spec)
 
     def create_bot_pod(
         self,
@@ -351,7 +351,7 @@ class BotPodCreator:
             )
         )
 
-        bot_pod_spec_data = self.apply_spec_to_bot_pod(bot_pod)
+        bot_pod_data = self.apply_spec_to_bot_pod(bot_pod)
 
         if add_webpage_streamer:
             # Create specific labels for the webpage streamer pod
@@ -381,7 +381,7 @@ class BotPodCreator:
         try:
             bot_pod_api_response = self.v1.create_namespaced_pod(
                 namespace=self.namespace,
-                body=bot_pod_spec_data
+                body=bot_pod_data
             )
 
             if add_webpage_streamer:
