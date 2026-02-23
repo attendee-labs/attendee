b6161720a1e0674e57c5226bb47b2ce546d010dd
Noah Duncan <crusty.noah@gmail.com>
2026-02-23 10:32:37 -0600

    Make separate env var for broker_transport_options

--- attendee/celery.py
+++ attendee/celery.py
@@ -1,3 +1,4 @@
+import json
 import os
 import ssl
 
@@ -9,7 +10,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attendee.settings")
 sslCertRequirements = None
 if os.getenv("DISABLE_REDIS_SSL"):
     sslCertRequirements = ssl.CERT_NONE
-elif os.getenv("REDIS_SSL_REQUIREMENTS") is not None and os.getenv("REDIS_SSL_REQUIREMENTS") != "":
+elif os.getenv("REDIS_SSL_REQUIREMENTS"):
     if os.getenv("REDIS_SSL_REQUIREMENTS") == "none":
         sslCertRequirements = ssl.CERT_NONE
     elif os.getenv("REDIS_SSL_REQUIREMENTS") == "optional":
@@ -24,20 +25,15 @@ if sslCertRequirements is not None:
         broker_use_ssl={"ssl_cert_reqs": sslCertRequirements},
         redis_backend_use_ssl={"ssl_cert_reqs": sslCertRequirements},
     )
-    # If we are sure that we are using SSL enable support for Redis Cluster hash
-    # tags. This is mainly to prevent CROSSSLOT errors when using Redis Cluster.
-    #
-    # https://github.com/celery/celery/issues/8276#issuecomment-3714489309
-    if sslCertRequirements == ssl.CERT_REQUIRED:
-        app.conf.update(
-            broker_transport_options={
-                "global_keyprefix": "{celeryattendee}:",
-                "fanout_prefix": True,
-                "fanout_patterns": True,
-            },
-        )
 else:
     app = Celery("attendee")
+
+# Currently the only use case for CELERY_BROKER_TRANSPORT_OPTIONS is to enable support for Redis Cluster hash
+# tags. This is mainly to prevent CROSSSLOT errors when using Redis Cluster (https://github.com/celery/celery/issues/8276#issuecomment-3714489309)
+# For this case set CELERY_BROKER_TRANSPORT_OPTIONS='{"global_keyprefix":"{celeryattendee}:","fanout_prefix":true,"fanout_patterns":true}'
+
+if os.getenv("CELERY_BROKER_TRANSPORT_OPTIONS"):
+    app.conf.update(broker_transport_options=json.loads(os.getenv("CELERY_BROKER_TRANSPORT_OPTIONS")))
 
 # Load configuration from Django settings
 app.config_from_object("django.conf:settings", namespace="CELERY")
