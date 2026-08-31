9156d57825c5702f0fb687c3669b72f1ddd07e6d
Noah Duncan <crusty.noah@gmail.com>
2026-08-31 09:47:34 -0500

    fix incorrect attribute

--- bots/teams_bot_adapter/teams_chromedriver_payload.js
+++ bots/teams_bot_adapter/teams_chromedriver_payload.js
@@ -2580,7 +2580,7 @@ const handleVideoTrack = async (event) => {
 
     let lastAudioFormat = null;  // Track last seen format
     const audioDataQueue = [];
-    const ACTIVE_SPEAKER_LATENCY_MS = window.initialData.perParticipantAudioUtteranceDelayMs;
+    const ACTIVE_SPEAKER_LATENCY_MS = window.teamsInitialData.perParticipantAudioUtteranceDelayMs;
     let trackIsNonSilent = false;
     let handleAudioTrackDebugInfo = {
         framesWithoutDominantSpeaker: 0,
