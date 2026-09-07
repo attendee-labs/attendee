a0a0889b97e852d6bbd03ca64cf2e96241cf899e
Noah Duncan <crusty.noah@gmail.com>
2026-09-06 21:23:34 -0500

    tc

--- bots/google_meet_bot_adapter/google_meet_bot_adapter.py
+++ bots/google_meet_bot_adapter/google_meet_bot_adapter.py
@@ -103,10 +103,6 @@ class GoogleMeetBotAdapter(WebBotAdapter, GoogleMeetUIMethods):
     def subclass_specific_after_bot_joined_meeting(self):
         self.after_bot_can_record_meeting()
 
-    def add_subclass_specific_chrome_options(self, options):
-        if self.google_meet_bot_login_should_be_used:
-            options.add_argument("--guest")
-
     def subclass_specific_chrome_policies(self):
         if not settings.ENFORCE_DOMAIN_ALLOWLIST_IN_CHROME:
             return {}
